771abc8cd8855a74171755f3525ea6aa98393130
Cody Mercer <me@codymercer.com>
2022-06-16 21:59:53 -0500

    refactor: mv savefile parser to ./src/save

--- src/test.ts
+++ src/test.ts
@@ -1,22 +1,31 @@
 import 'module-alias/register'
 import fs from 'fs'
 import path from 'path'
-import parse from '@/index'
+import parse from '@/save/index'
 
-const CHARACTER = 'dclone'
 
 const log = (name: string) => path.resolve(__dirname, '..', name)
-const character = (name: string) => path.resolve(__dirname, '../data', name)
+const character = (name?: string) => path.resolve(__dirname, '../data', name ? name : '')
 const file = (name: string) => fs.readFileSync(character(name))
 
-try {
-  const data = parse(file(CHARACTER))
-  const json = JSON.stringify(data, undefined, 2)
+const files = fs.readdirSync(character())
 
-  fs.writeFileSync(log(`output/${CHARACTER}.json`), json)
-} catch (error) {
-  console.error((error as Error)?.stack)
-  fs.writeFileSync(log('error.log'), (error as Error).toString())
-  throw error
+const parseSaveFile = (name: string) => {
+  try {
+    const buffer = file(name)
+
+    console.log('parsing savefile:', name)
+    console.log(buffer)
+
+    const data = parse(buffer)
+    const json = JSON.stringify(data, undefined, 2)
+
+    fs.writeFileSync(log(`output/${name}.json`), json)
+  } catch (error) {
+    console.error((error as Error)?.stack)
+    fs.writeFileSync(log('error.log'), (error as Error).toString())
+  }
 }
+
+files.forEach(parseSaveFile)
 
